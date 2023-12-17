8de8c5d23e8bb616a2139c8c1ae62f97db259a2b
kalin <kalin.stanchev86@gmail.com>
2023-12-17 09:30:03 +0200

    add cube example

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './main.scss';
 
 // import './examples/coordsys.js';
 
-// import './examples/cube.js';
+import './examples/cube.js';
 
-import './examples/dotProduct.js';
+// import './examples/dotProduct.js';
 
